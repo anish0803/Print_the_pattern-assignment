41f0a303a3595d8ead74f149374ebc068e284895
anish0803 <singhanish124@gmail.com>
2022-07-01 11:39:42 +0530

    q1 commited

---
+++ q1.js
@@ -0,0 +1,32 @@
+// QNo 1. Print the following Pattern.
+// Input
+// You will be given a number n
+// 1 <= n <= 100
+// Output
+// Print the following pattern
+// Sample Output:
+// For n = 5
+
+// *                 *
+//      *        *
+//          *
+//      *        *
+// *                 *
+
+
+let num = 5;
+
+for(let i = 1; i<=5; i++){
+  let stars = ``
+  for(let j = 1; j<=5; j++){
+  if(i==j || (i+j==6)){
+     stars += "   *    "
+  }
+  else{
+    stars += "       "
+  }
+  
+  
+  }
+  console.log(stars)
+}
