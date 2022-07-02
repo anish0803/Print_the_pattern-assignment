987d303a74940d234fcbc2617c5618f1495f4656
anish0803 <singhanish124@gmail.com>
2022-07-02 23:38:19 +0530

    q1 commited

--- q1.js
+++ q1.js
@@ -14,19 +14,33 @@
 // *                 *
 
 
-let num = 5;
+// let num = 5;
 
-for(let i = 1; i<=5; i++){
-  let stars = ``
-  for(let j = 1; j<=5; j++){
-  if(i==j || (i+j==6)){
-     stars += "  *  ";
-  }
-  else{
-    stars += "     "
-  }
+// for(let i = 1; i<=5; i++){
+//   let stars = ``
+//   for(let j = 1; j<=5; j++){
+//   if(i==j || (i+j==6)){
+//      stars += "  *  ";
+//   }
+//   else{
+//     stars += "     "
+//   }
   
   
+//   }
+//   console.log(stars)
+// }
+
+// -------------------->>------------
+
+for (let i = 1; i <= 5; i++) {
+  let stars = " ";
+  for (let j = 1; j <= 5; j++) {
+    if (i == j || i + j == 6) {
+      stars += " * ";
+    } else if (i != j) {
+      stars += "   ";
+    }
   }
-  console.log(stars)
+  console.log(stars);
 }
